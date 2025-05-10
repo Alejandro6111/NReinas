f2d61cf84d267439f52f3d3317ae8076a80b6515
Alejandro6111 <alejandro.soche10@gmail.com>
2025-05-10 17:12:11 -0500

    Se deja listo tablero, se empieza a trabajar en las soluciones

--- src/model/SolucionadorReinas.java
+++ src/model/SolucionadorReinas.java
@@ -1,5 +1,31 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolucionadorReinas {
+    private Tablero tableroActual;
+    private List<int[][]> solucionesEncontradas;
+    private int tamanoTablero;
+    private int filaInicialFija = -1; // Indica que no hay fila inicial fija
+    private int columnaInicialFija = -1; // Indica que no hay columna inicial fija
+
+    // Constructor
+    public SolucionadorReinas(int tamano){
+        this.tamanoTablero = tamano;
+        this.solucionesEncontradas = new ArrayList<>();
+        // El tablero se crea cuando se inicie la resolucion para una reina inicial
+    }
+
+    /**
+     * Metodo principal para iniciar la busqueda solucones
+     * El usuario ingresa la posicion de la primera Reina
+     * @param filaReinaInicial Fila donde el usuiario comoloca la primera reina
+     * @param columnaReinaInicial Columna donde el usuario coloca la segunda reina
+     */
+    public void mEncontrarSolucionesConReinaInicial(int filaReinaInicial, int columnaReinaInicial){
+        this.solucionesEncontradas.clear(); // Limpiar soluciones previas
+        this.tableroActual = new Tablero(this.tamanoTablero);
+    }
     
 }
--- src/model/Tablero.java
+++ src/model/Tablero.java
@@ -28,7 +28,7 @@ public class Tablero {
     
     // Metodo para quitar una reina 
     public void mQuitarReina(int fila, int columna){
-        if (mEsPosicionValida){
+        if (mEsPosicionValida(fila, columna)){
             this.tablero[fila][columna] = 0; // Marcamos como vacia 
         }
     }
@@ -46,10 +46,84 @@ public class Tablero {
         }
 
         // Verificar fila hacia la izquiera
+        for (int c = 0; c < columna; c++){
+            if (this.tablero[fila][c] == 1){
+                return false;
+            }
+        }
+        // Verificar la fila hacia la derecha
+        for (int c = columna + 1; c < this.tamano; c++){
+            if (this.tablero[fila][c] == 1){
+                return false;
+            }
+        }
+        // Verificar columna entera 
+        for (int r = 0; r < this.tamano; r++){
+            if (r == fila) continue; // Para no recorrer la misma casilla
+            if (this.tablero[r][columna] == 1){
+                return false;
+            }
+        }
+        // Verificar diagonal superrior izquierda   
+        for (int r = fila - 1, c = columna -1; r >= 0 && c >= 0; r--, c--){
+            if (this.tablero[r][c] == 1){
+                return false;
+            }
+        }
+        // Verificar diagonal inferior izquierfa
+        for (int r = fila + 1, c = columna -1; r < this.tamano && c >= 0; r++, c--){
+            if (this.tablero[r][c] == 1){
+                return false;
+            }
+        }
+        // Verificar diagonal superor derecha
+        for (int r = fila - 1, c = columna + 1; r >= 0 && c < this.tamano; r--, c++){
+            if (this.tablero[r][c] == 1){
+                return false;
+            }
+        }
+        // Verificar diagonal inferior derecha
+        for (int r = fila + 1, c = columna + 1; r < this.tamano && c < this.tamano; r++, c++){
+            if (this.tablero[r][c] == 1){
+                return false;
+            }
+        }
+        return true;
     }
 
+    // Metodo para obtener una representacion del tablero 
+    public int[][] mGetTableroState(){
+        int[][] copiaTablero = new int[this.tamano][this.tamano];
+        for (int i = 0; i < this.tamano; i++){
+            System.arraycopy(this.tablero[i], 0 , copiaTablero [i], 0 , this.tamano);
+        }
+        return copiaTablero;
+    }
 
+    // Getter para el tamaño 
+    public int mGeTamano(){
+        return this.tamano;
+    }
 
+    // Metodo simple para impromir el tablero en la consola
+    public void mImprimirTableroConsola(){
+        System.out.println("Estado actual del tablero: ");
+        for (int i = 0; i < tamano; i++){
+            for (int j = 0; j < tamano; j++){
+                System.out.println(this.tablero[i][j] + "");
+            }
+            System.out.println();
+        }
+        System.out.println("---------------------");
+    }
+
+    // Metodo para obtener el valor de una celda espeficica 
+    public int mGetValorCelda(int fila, int columna){
+        if (mEsPosicionValida(fila, columna)){
+            return this.tablero[fila][columna];
+        }
+        return -1;
+    }
 
     
 }
